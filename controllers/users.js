9c7845c65bcc6e1d7b43a527157e087646868f88
Maks Subochev <maxim.subochev@yandex.ru>
2023-06-19 22:45:57 +0300

    fix: коды ошибок для контроллера пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -43,7 +43,7 @@ const updateProfile = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(userById, { name, about }, { new: true, runValidators: true })
     .then((user) => {
-      res.status(201).send({ data: user });
+      res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
@@ -58,7 +58,7 @@ const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(userById, { avatar }, { new: true, runValidators: true })
     .then((user) => {
-      res.status(201).send({ data: user });
+      res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
