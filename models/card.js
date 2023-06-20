7340f5440c82cba36c0370057eb3a2ab48514971
Maks Subochev <maxim.subochev@yandex.ru>
2023-06-20 19:58:57 +0300

    fix: автотесты

--- models/card.js
+++ models/card.js
@@ -2,34 +2,35 @@ const mongoose = require('mongoose');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const validator = require('validator');
 
-const cardSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => validator.isURL(v),
+const cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => validator.isURL(v),
+      },
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      default: [],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  likes: {
-    type: [mongoose.Schema.Types.ObjectId],
-    default: [],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-},
-{ versionKey: false },
+  { versionKey: false },
 );
 
 module.exports = mongoose.model('card', cardSchema);
--- models/user.js
+++ models/user.js
@@ -2,29 +2,29 @@ const mongoose = require('mongoose');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const validator = require('validator');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: [2, 'минимальная длина поля "name" - 2'],
-    maxlength: [30, 'максимальная длина поля "name" - 30'],
-    required: [true, 'поле "name" должно быть заполнено'],
-  },
-  about: {
-    type: String,
-    minlength: [2, 'минимальная длина поля "about" - 2'],
-    maxlength: [30, 'максимальная длина поля "about" - 30'],
-    required: [true, 'поле "about" должно быть заполнено'],
-  },
-  avatar: {
-    type: String,
-    required: [true, 'поле "avatar" должно быть заполнено'],
-    validate: {
-      validator: (v) => validator.isURL(v),
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: [2, 'минимальная длина поля "name" - 2'],
+      maxlength: [30, 'максимальная длина поля "name" - 30'],
+      required: [true, 'поле "name" должно быть заполнено'],
     },
-
+    about: {
+      type: String,
+      minlength: [2, 'минимальная длина поля "about" - 2'],
+      maxlength: [30, 'максимальная длина поля "about" - 30'],
+      required: [true, 'поле "about" должно быть заполнено'],
+    },
+    avatar: {
+      type: String,
+      required: [true, 'поле "avatar" должно быть заполнено'],
+      validate: {
+        validator: (v) => validator.isURL(v),
+      },
+    },
   },
-},
-{ versionKey: false },
+  { versionKey: false },
 );
 
 module.exports = mongoose.model('user', userSchema);
