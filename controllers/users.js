c0cc059fa6400858ab282db31df1a4328bb398c4
Maks Subochev <maxim.subochev@yandex.ru>
2023-06-26 17:09:25 +0300

    feat: хеширование пароля в контроллере createUser

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 const getUsers = (req, res) => {
@@ -27,9 +28,19 @@ const getUserById = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, about, avatar, email, password } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => { res.status(201).send({ data: user }); })
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({
+        name, about, avatar, email, password: hash,
+      });
+    })
+    .then((user) => {
+      res.status(201).send({ data: user });
+    })
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(400).send({ message: 'переданы некорректные данные при создании пользователя' });
