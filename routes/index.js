955fa9a2c5785bf1c7ee9f082f5f8c0723e2b7ca
Maks Subochev <maxim.subochev@yandex.ru>
2023-06-28 14:51:54 +0300

    feat: создать роуты для sign in и sign up

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./card');
+const { login, createUser } = require('../controllers/users');
 
 router.get('/', (req, res) => {
   res.status(200);
   res.send('hello');
 });
 
+router.post('/signin', login);
+router.post('/signup', createUser);
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 router.use('*', (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const {
-  getUsers, getUserById, createUser, updateProfile, updateAvatar,
+  getUsers, getUserById, updateProfile, updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:_id', getUserById);
-router.post('/', createUser);
 router.patch('/me', updateProfile);
 router.patch('/me/avatar', updateAvatar);
 
