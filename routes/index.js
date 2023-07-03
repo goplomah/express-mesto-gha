5b760224110484b6e0cdbee0e7bd800f99101299
Maks Subochev <maxim.subochev@yandex.ru>
2023-07-03 14:02:03 +0300

    feat: регулярка для аватара

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.post('/signup', celebrate({
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(/https?:\/\/(www\.)?([A-Za-z0-9-.]+)\.([A-Za-z0-9]+)([A-Za-z0-9-._~:/?#@!$&'()*+,;=]*)#?/),
   }),
 }), createUser);
 router.use('/users', auth, userRouter);
